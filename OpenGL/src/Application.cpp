b43584b08790757315a071e1bfa666a82588e19e
SFaceNWB <113598232+SFaceNWB@users.noreply.github.com>
2025-10-22 16:24:28 +0800

    OpenGL中的Uniform(shader转换为C++)

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -7,9 +7,9 @@
 #include <sstream>
 
 #define ASSERT(x) if(!(x)) __debugbreak();
-#define GLCall(x)  { GLClearError();\
+#define GLCall(x)  GLClearError();\
     x;\
-    ASSERT(GLLogCall(#x, __FILE__, __LINE__)) }
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
 
 static void GLClearError()
 {
@@ -118,7 +118,6 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-
     
 
     /* Create a windowed mode window and its OpenGL context */
@@ -129,8 +128,11 @@ int main(void)
         return -1;
     }
 
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+	glfwSwapInterval(1);
 
     if (glewInit() != GLEW_OK)
     {
@@ -169,13 +171,28 @@ int main(void)
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     glUseProgram(shader);
 
+	GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+	ASSERT(location != -1);
+	GLCall(glUniform4f(location, 0.6f, 0.3f, 0.6f, 1.0f));
+
+	float r = 0.0f;
+	float increment = 0.05f;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        GLCall(glUniform4f(location, r, 0.3f, 0.6f, 1.0f));
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+        if(r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+			increment = 0.05f;
+
+		r += increment;
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
