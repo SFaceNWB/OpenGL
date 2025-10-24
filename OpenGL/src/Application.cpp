eadf910a25def3753693a75d2cb0af4273915b3f
SFaceNWB <113598232+SFaceNWB@users.noreply.github.com>
2025-10-24 18:21:33 +0800

    创建TestTexture

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -23,6 +23,7 @@
 #include "imgui/imgui_impl_opengl3.h"
 
 #include "tests/TestClearColor.h"
+#include "tests/TestTexture2D.h"
 
 int main(void)
 {
@@ -73,6 +74,7 @@ int main(void)
 		currentTest = testMenu;
 
 		testMenu->RegisterTest<test::TestClearColor>("Clear Color");
+		testMenu->RegisterTest<test::TestTexture2D>("2D Texture");
 
 
         while (!glfwWindowShouldClose(window))
