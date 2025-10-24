1216c3a46132ed3110deecbd66dca4500fab77a4
SFaceNWB <113598232+SFaceNWB@users.noreply.github.com>
2025-10-24 17:32:03 +0800

    创建Tests

--- OpenGL/src/tests/TestClearColor.cpp
+++ OpenGL/src/tests/TestClearColor.cpp
@@ -15,10 +15,10 @@ namespace test {
 	}
 	void TestClearColor::OnUpdate(float deltaTime)
 	{
+	
 	}
 	void TestClearColor::OnRender()
 	{
-
 		GLCall(glClearColor(m_ClearColor[0], m_ClearColor[1], m_ClearColor[2], m_ClearColor[3]));
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 	}
